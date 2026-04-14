91cc6ebe1de4a7cdec34b82f0977ff65231331b5
Mathews Baby <mathewskb@gmail.com>
2026-04-14 19:10:47 +0530

    cleanup

--- Serilog.AWS.Logging/Controllers/AWSSecretController.cs
+++ Serilog.AWS.Logging/Controllers/AWSSecretController.cs
@@ -12,30 +12,39 @@ namespace Serilog.AWS.Logging.Properties
 
         public async Task<IActionResult> GetSecretAsync()
         {
-
-            _logger.LogDebug("Received Request");
-            _logger.LogInformation("Processing request");
-
-            var request = new GetSecretValueRequest
+            try
             {
-                SecretId = _config["AWSSecretName"]
-            };
-            
-            _logger.LogInformation("Processing request");
-            _logger.LogDebug("Sending request to AWS Secrets Manager");
+                _logger.LogDebug("Received Request");
+                _logger.LogInformation("Processing request");
 
-            var response = await _secretsManager.GetSecretValueAsync(request);
-            _logger.LogDebug("Received response from AWS Secrets Manager");
-            _logger.LogInformation("Processing response");
+                var request = new GetSecretValueRequest
+                {
+                    SecretId = _config["AWSSecretName"]
+                };
 
-            if (!string.IsNullOrEmpty(response.SecretString))
+                _logger.LogInformation("Processing request");
+                _logger.LogDebug("Sending request to AWS Secrets Manager");
+
+                var response = await _secretsManager.GetSecretValueAsync(request);
+                _logger.LogDebug("Received response from AWS Secrets Manager");
+                _logger.LogInformation("Processing response");
+
+                if (!string.IsNullOrEmpty(response.SecretString))
+                {
+                    _logger.LogInformation("Secret retrieved successfully");
+                    return Ok(response.SecretString);
+                }
+
+
+            }
+            catch (System.Exception)
             {
-                _logger.LogInformation("Secret retrieved successfully");
-                return Ok(response.SecretString);
+                _logger.LogError("Failed to retrieve secret");
+                throw new Exception("Secret is empty");
             }
 
-            _logger.LogError("Failed to retrieve secret");
-            throw new Exception("Secret is empty");
+            _logger.LogWarning("Secret is empty");
+            return NotFound("Secret is empty");
         }
     }
 }
